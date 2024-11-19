2aff7cbc0ab4c22d33f0d27ba3d9ed9e2d2b2e5c
Eyder Suarez <ey_der19@hotmail.com>
2024-11-19 16:52:45 -0500

    Configuracion supabase

---
+++ src/app/services/supabase.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { SupabaseClient, createClient } from '@supabase/supabase-js'
+import { environment } from '../../environments/environment';
+
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SupabaseService {
+
+public supabaseClient: SupabaseClient;
+
+constructor() {
+
+  this.supabaseClient = createClient(
+    environment.SUPABASE_URL, environment.SUPABASE_KEY
+  )
+
+}
+
+
+
+
+
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  SUPABASE_URL: "https://ymkrpndbehhpwbebbfpy.supabase.co",
+  SUPABASE_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inlta3JwbmRiZWhocHdiZWJiZnB5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzE2MTQ3NjQsImV4cCI6MjA0NzE5MDc2NH0.JELWjCIJSrdaUitfxTHHbjwAlrup5uuPynbCB7Y36Ik",
+  authorization: "",
+}
