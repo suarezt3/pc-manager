eed2822d17bd46ea5611433ce1331e0ac37ffa6c
Eyder Suarez <ey_der19@hotmail.com>
2024-12-30 12:44:35 -0500

    Se avanza en el metodo para subir el PDF

--- src/app/components/form-alistamientos/form-alistamientos.component.ts
+++ src/app/components/form-alistamientos/form-alistamientos.component.ts
@@ -2,6 +2,9 @@ import { CommonModule } from '@angular/common';
 import { Component, OnInit, inject } from '@angular/core';
 import { ReactiveFormsModule, FormsModule, FormGroup, FormBuilder } from '@angular/forms';
 import { NgZorroModule } from '../../ng-zorro/ng-zorro.module';
+import { NzMessageService } from 'ng-zorro-antd/message';
+import { NzUploadChangeParam, NzUploadModule } from 'ng-zorro-antd/upload';
+import { DataService } from '../../services/data.service';
 
 @Component({
   standalone: true,
@@ -16,10 +19,22 @@ export class FormAlistamientosComponent implements OnInit {
   isEnglish = false;
 
   public formAlistamientos!: FormGroup;
+  selectedFile: File | null = null;
 
   constructor() { }
 
   public fb = inject(FormBuilder);
+  public messageService = inject(NzMessageService);
+  public dataService = inject(DataService);
+
+
+  onFileSelected(event: Event) {
+    const input = event.target as HTMLInputElement;
+    if (input.files && input.files.length > 0) {
+      this.selectedFile = input.files[0];
+    }
+  }
+
 
 
   ngOnInit() {
@@ -35,11 +50,62 @@ export class FormAlistamientosComponent implements OnInit {
       plate: [''],
       model_pc: [''],
       ticket: ['']
-  });
+   });
+
+  }
+
+
+  async uploadFile() {
+    if (this.selectedFile) {
+      try {
+        const result = await this.dataService.uploadDocument(this.selectedFile);
+        console.log('File uploaded successfully:', result);
+      } catch (error) {
+        console.error('Error uploading file:', error);
+      }
+    }
+  }
+
+
+
+  // handleChange(event: NzUploadChangeParam): void {
+  //   if (event.file.status !== 'uploading') {
+  //     console.log( "DOCUMENTO", event.file, event.fileList);
+  //     this.dataService.uploadDocument(event.file)
+  //     .then((res) => {
+  //       console.log("RES", res);
+  //     })
+  //     .catch((err) => {
+  //       console.log("ERROR", err);
+  //     });
+  //   }
+  //   if (event.file.status === 'done') {
+  //     this.messageService.success(`${event.file.name} file uploaded successfully`);
+  //   } else if (event.file.status === 'error') {
+  //     this.messageService.error(`${event.file.name} file upload failed.`);
+  //   }
+  // }
+
+
+  enviar() {
 
 
 
 
+    const id_tecnico = JSON.parse(localStorage.getItem('id_user') || 'null');
+    const dataForm = {
+      id_tecnico: id_tecnico,
+      date: this.formAlistamientos.get('date')?.value,
+      description: this.formAlistamientos.get('description')?.value,
+      document_acta: this.formAlistamientos.get('document_acta')?.value,
+      opco: this.formAlistamientos.get('opco')?.value,
+      usuario: this.formAlistamientos.get('usuario')?.value,
+      serial: this.formAlistamientos.get('serial')?.value,
+      plate: this.formAlistamientos.get('plate')?.value,
+      model_pc: this.formAlistamientos.get('model_pc')?.value,
+      ticket: this.formAlistamientos.get('ticket')?.value
+    };
 
+    console.log(dataForm);
   }
 }
--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -33,6 +33,17 @@ constructor() {
 
 
  /**
+ *
+ * @param data ensertar datos de un usuario en la base de datos
+ * @returns
+ */
+ async createalistamiento(data: any) {
+  const { data: user, error } = await this.supabaseClient.from('alistamientos').insert(data);
+  return { user, error }; // Devuelve los datos y el error, si es necesario
+}
+
+
+ /**
   *
   * @param
   * @returns Para traer la informacion del usuario al iniciar sesion
@@ -43,10 +54,18 @@ constructor() {
   console.log("USERDATA", this.userData[0]);
   // Guarda los datos en localStorage
   localStorage.setItem('userData', JSON.stringify(this.userData));
+  localStorage.setItem('id_user', JSON.stringify(this.userData[0].id));
 
   return { perfil, error }; // Devuelve los datos y el error, si es necesario
 
 }
+
+
+async uploadDocument(doc: any) {
+  const { data: docUpload, error } = await this.supabaseClient.storage.from('actas').upload('Acta.pdf', doc);
+  return { docUpload, error }; // Devuelve los datos y el error, si es necesario
+}
+
 
 
 }
